2d76ffe95157e47a8fe88eaf8ddc37c9e16b9873
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2019-02-22 09:55:17 -0800

    Sentry.io setting for circleci

--- config/sentry.php
+++ config/sentry.php
@@ -1,14 +1,19 @@
 <?php
-
+$env = getenv("APP_ENV");
+if(!$env || $env == 'testing')
+{
+    return array(
+        'dsn' => 'https://b53658ff4ec749719da39905217d41e0@sentry.io/1366253',
+        'breadcrumbs.sql_bindings' => true,
+        'user_context' => false,
+    );
+}
 return array(
     'dsn' => '___DSN___',
-
     // capture release as git sha
     // 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
-
     // Capture bindings on SQL queries
     'breadcrumbs.sql_bindings' => true,
-
     // Capture default user context
     'user_context' => false,
 );
