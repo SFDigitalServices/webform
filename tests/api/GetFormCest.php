6da35f5168c813445183f58bf40ebc89df3cc8e8
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2019-03-22 16:33:33 -0700

    fix more test

--- tests/api/GetFormCest.php
+++ tests/api/GetFormCest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class GetFormCest
 {
@@ -11,18 +11,18 @@ class GetFormCest
     }
 
      // tests
-       
+
     public function getFormViaAPI(\ApiTester $I){
         $I->sendPOST('/form/getApiToken', $this->userAttributes);
         $response = json_decode($I->grabResponse());
-        
+
         $I->haveHttpHeader('authorization', 'Bearer '. $response->api_token);
         $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
-        $I->sendPOST('/form/getForm', ['form_id' => 1]);
+        $I->sendPOST('/form/getForm', ['form_id' => 12]);
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
         $I->seeResponseIsJson();
 
-        $I->seeResponseContains('{"id":1');
+        $I->seeResponseContains('{"id":12');
     }
 
 }
