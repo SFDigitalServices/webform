75b4f9cd7536ff7fcf76ce0f0e8bc51c21dae23d
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2019-04-12 16:49:50 -0700

    Email Service

---
+++ app/Mail/FormBuilderMailer.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class FormBuilderMailer extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        $address = 'henry.jiang@sfgov.org'; //FROM address
+        $subject = 'This is a demo!';
+        $name = 'Jane Doe';
+
+        $headerData = [
+            //Categories in SendGrid allow you to split your statistics into sections. For example,
+            //if you have a Whitelabeled service, you can split your statistics by the user login.
+            'category' => 'category',
+            'unique_args' => [
+                'variable_1' => 'abc'
+            ]
+        ];
+
+        $header = $this->asString($headerData);
+
+        $this->withSwiftMessage(function ($message) use ($header) {
+            $message->getHeaders()
+                    ->addTextHeader('X-SMTPAPI', $header);
+        });
+
+        return $this->view('emails.template')
+                    ->from($address, $name)
+                    ->cc($address, $name)
+                    ->bcc($address, $name)
+                    ->replyTo($address, $name)
+                    ->subject($subject);
+    }
+
+    private function asJSON($data)
+    {
+        $json = json_encode($data);
+        $json = preg_replace('/(["\]}])([,:])(["\[{])/', '$1$2 $3', $json);
+
+        return $json;
+    }
+
+
+    private function asString($data)
+    {
+        $json = $this->asJSON($data);
+
+        return wordwrap($json, 76, "\n   ");
+    }
+}
---
+++ config/mail.php
@@ -0,0 +1,24 @@
+<?php
+    return [
+'driver' => env('MAIL_DRIVER', 'smtp'),
+'host' => env('MAIL_HOST'),
+'port' => env('MAIL_PORT'),
+'from' => [
+    'address' => env('MAIL_FROM_ADDRESS'),
+    'name' => env('MAIL_FROM_NAME'),
+    ],
+'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+'username' => env('MAIL_USERNAME'),
+
+'password' => env('MAIL_PASSWORD'),
+'sendmail' => '/usr/sbin/sendmail -bs',
+
+'markdown' => [
+    'theme' => 'default',
+
+    'paths' => [
+        resource_path('views/vendor/mail'),
+    ],
+],
+
+];
