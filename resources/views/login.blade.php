a95190ada3c8370684bf791771e366cd6f3ff715
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-05-22 22:12:41 -0700

    Refactor login page markup to use Bootstrap

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,57 +1,67 @@
 @extends('layouts.app')
 @section('content')
-<link rel="stylesheet" href="/assets/css/toolkit.css">
 <style>
-	body {background:#222}
+	html,
+	body {
+		height: 100%;
+		margin: 0 !important;
+		padding: 0 !important;
+	}
+
+	body {
+		background:#222 url(/assets/images/bg4.jpg) no-repeat center center;
+		background-size: 100% 100%;
+		min-height: 100%;
+	}
+
+	.row {
+		margin-top: 6em;
+	}
+
+	header,
+	h1 {
+		font-size: 2em;
+		color: #fff;
+		text-shadow: 0 0 18px #000;
+	}
+
+	h2 {
+		margin-top: 0;
+	}
+
+	.panel {
+		padding: 2em;
+	}
 </style>
-<header class="hero-header text-center">
-	<h1 class="hero-title c-white t-shadow">Webform Builder</h1>
+	<div class="container container-fluid">
+		<div class="row">
+			<header class="col-md-8 col-md-offset-2">
+				<h1 class="text-center">Webform Builder</h1>
 
-	<div class="row">
-		<div class="medium-9 medium-centered columns">
-			<p class="delta t-sans t-shadow c-white">City and County of San Francisco - Digital Services.</p>
-			<br/><br/><br/>
+				<p class="text-center">City and County of San Francisco - Digital Services</p>
+			</div>
+		</header>
 
-			<section class="row margin-bottom--2x">
-				<div class="medium-10 medium-centered columns">
-					
-				<form class="form-card margin-bottom--2x" id="login-form" action="/home" method="POST">
-						<div class="row">
-							<div class="medium-12 columns padding-left padding-right padding-top padding-bottom">
-								<header class="margin-bottom">
-									<h3 class="t-serif gamma">Login</h3>
-								</header>
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="panel">
+					<form class="panel-body" id="login-form" action="/home" method="POST">
+						<h2>Login</h2>
 
-								<div class="form-email">
-									<label for="email" class="">Email Address</label>
-									<input name="email" type="email" placeholder="first.last@sfgov.org" class="" />
-								</div>
-
-								<div class="form-password">
-									<label for="password" class="">Password</label>
-									<input name="password" type="password" placeholder="Password" class="" />
-								</div>
-
-								<div class="form-submit">
-									<button class="button primary expand-small">Continue</button>
-								</div>
-							</div>
+						<div class="form-group">
+							<label for="email">Email Address</label>
+							<input name="email" type="email" placeholder="first.last@sfgov.org" class="form-control input-lg" />
 						</div>
 
+						<div class="form-group">
+							<label for="password">Password</label>
+							<input name="password" type="password" placeholder="Password" class="form-control input-lg" />
+						</div>
+
+						<button class="btn btn-primary btn-lg">Continue</button>
 					</form>
-
-					<!--div class="note text-center bg-smoke padding margin-top">
-						<p class="t-small no-margin">Forgot your email or password?
-							<a target="_blank" href="https://bifrost.sfgov.org/" class="button-link t-small">DT IAM Support</a>
-						</p>
-					</div-->
 				</div>
-			</section>
-
+			</div>
 		</div>
-	</div>
 </header>
-
-<script type="text/javascript" language="Javascript" src="/assets/js/toolkit.js"></script>
-
 @endsection
