e9e56346ec87d61eb72bc80da45f2482d1469c04
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-05-24 09:41:12 -0700

    DFB-326: Remove / streamline animations (#87)
    * Remove most animations, speed up CSV animation
    
    * Standardize loading spinner styles on welcome page
    
    * Toggle accordion sections on click
    
    * [unrelated] Center the “Welcome back!” text
    
    * Show pointer cursor when clicking on accordions

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -7,7 +7,7 @@
 		  var api_token = '<?php echo $api_token;?>';
 
 		  $(document).ready(function(){
-			  $(".content").show(1500);
+			  $(".content").show();
 
 			  callAPI("/form/getForms", {}, loadHome);
 			});
@@ -15,7 +15,7 @@
 			//$(window).unload(function(){});
 
 			window.onpopstate = function (event) {
-				$('.container').hide('fast');
+				$('.container').hide();
 				if (event.state) {
 					if (event.state.formId) {
 						loadContent(event.state.formId);
@@ -39,7 +39,7 @@
 		<div class="content" style="display:none">
             <div class="hidden-xs col-sm-1 col-md-2 col-lg-3">&nbsp;</div>
 			<div class="col-xs-12 col-sm-10 col-md-8 col-lg-6">
-				<h1 class="welcomeBack">Welcome back <?php print $name; ?>!</h1>
+				<h1 class="welcomeBack text-center">Welcome back, <?php print $name; ?>!</h1>
 				<div class="welcomeBox">
 					<div>
 						<a href="javascript:void(0)" onclick="loadContent()" class="btn btn-info btn-lg btn-block">Create a New Form</a>
@@ -1219,7 +1219,7 @@
 				<div class="clickMenu">
 					<ul>
 						<span class="saveStatus"></span> 
-						<i class="fas fa-circle-notch fa-spin saveSpinner" style="display:none;color:#aaa"></i>
+						<i class="fas fa-circle-notch fa-spin saveSpinner" style="display:none;"></i>
 						<li onkeydown="if(event.keyCode == 13) confirmAction('clone','doAction.php?action=clone')" onclick="javascript:confirmAction('clone','doAction.php?action=clone')" tabindex="0" data-toggle="tooltip" title="Clone"><i class="fas fa-clone"></i></li>
 						<li onkeydown="if(event.keyCode == 13) confirmAction('delete','doAction.php?action=delete')" onclick="javascript:confirmAction('delete','doAction.php?action=delete')" tabindex="0" data-toggle="tooltip" title="Delete"><i class="fas fa-trash"></i></li>
 						<li onkeydown="if(event.keyCode == 13) confirmAction('exit','editor.php')" onclick="javascript:confirmAction('exit','editor.php')" data-toggle="tooltip" tabindex="0" title="Exit"><i class="fas fa-sign-out-alt"></i></li>
