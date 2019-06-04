7e8cd2640169ec48c785a10f2603816c1f7e7e43
Jim Brodbeck <jim.brodbeck@sfgov.org>
2019-06-04 10:34:39 -0700

    DFB-315 rollback to old validation library so it does not require validation on non-visible fields

--- public/assets/js/fb.js
+++ public/assets/js/fb.js
@@ -1197,7 +1197,7 @@ function embedCode(id) {
 	var str = "<!-- If possible, place the following in your <head> tag. -->"+
 	"\n"+
 	"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>\n"+
-	"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js\"></script>\n"+
+	"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.10.1/validator.min.js\"></script>\n"+
 	"<script src=\""+assetsUrl+"js/error-msgs.js\"></script>"+
 	"\n"+
 	"<link href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\" rel=\"stylesheet\" />\n"+
