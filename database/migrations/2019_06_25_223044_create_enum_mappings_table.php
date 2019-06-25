7a6d31a7eb26ad80f1252bdd5a7c0cebf4cba948
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2019-06-25 15:40:14 -0700

    add database migration to create lookup table

--- database/migrations/2019_06_25_223044_create_enum_mappings_table.php
+++ database/migrations/2019_06_25_223044_create_enum_mappings_table.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace App\Migrations;
+namespace App\Migration;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEnumMappingsTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +14,32 @@ class CreateEnumMappingsTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('users');
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('api_token');
+            $table->timestamps();
+        });
+
+        Schema::dropIfExists('user_form');
+        Schema::create('user_form', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('form_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::dropIfExists('forms');
+        Schema::create('forms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('content');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
         Schema::dropIfExists('enum_mappings');
         Schema::create('enum_mappings', function (Blueprint $table) {
             $table->increments('id');
@@ -31,6 +56,6 @@ class CreateEnumMappingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('enum_mappings');
+        Schema::drop('users');
     }
 }
