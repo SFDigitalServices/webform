a97a4c26f4a8194a950bd46e56b8729535058ebd
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2019-06-25 18:44:49 -0700

    css compilation

---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+const mix = require('laravel-mix');
+
+mix.sass('public/assets/sass/app.scss', 'public/assets/css');
+/*
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
+*/
