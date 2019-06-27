62d71e2f45495810f1b8c0a600af08d3c88292a0
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-06-26 20:51:09 -0700

    Convert editor.css to nested SCSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 		<link href="/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
 		<link href="/assets/css/bootstrap-tagsinput.css" rel="stylesheet">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-		<link href="/assets/css/editor.css" rel="stylesheet">
+		<link href="/assets/css/app.css" rel="stylesheet">
 
 		<!--[if lt IE 9]>
 		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
