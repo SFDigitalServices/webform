91a4b2e3fa84d89af77c5f88d481aa020adb831a
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-07-09 13:01:56 -0700

    Remove unused prep-text field

--- resources/views/partials/fields/prep-text.blade.php
+++
@@ -1,8 +0,0 @@
-<label class=" control-label valtype" data-valtype="label">Prepended text</label>
-<div class="">
-	<div class="input-group">
-	  <span class="input-group-addon valtype" data-valtype="prepend">^_^</span>
-	  <input class="form-control valtype" placeholder="" id="prependedInput" type="text" data-valtype="placeholder">
-	</div>
-	<p class="help-block valtype" data-valtype="help"></p>
-</div>
