e1c8115db164e5ec5eb7e635802b8f96ab78b276
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-07-09 13:01:49 -0700

    Convert tabs to spaces

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,11 @@
 <div class="header">
   <div class="header-container">
-		<div class="logo">
-			<a href="#" onclick="javascript:event.preventDefault(); confirmAction('exit','editor.php')">
+    <div class="logo">
+      <a href="#" onclick="javascript:event.preventDefault(); confirmAction('exit','editor.php')">
         <img src="/assets/images/SF_Digital_Services-logo.png" alt="SF Digital Services Logo" />
       </a>
     </div>
-		Webform Builder
+    Webform Builder
     <a href="/home" class="header-sign-out btn btn-default">Sign Out</a>
   </div>
 </div>
