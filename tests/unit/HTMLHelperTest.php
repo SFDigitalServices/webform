75ccf5da98d2524e15d624d314e6ccaee7115fcd
Jim Brodbeck <jim.brodbeck@sfgov.org>
2019-07-11 09:48:34 -0700

    unit tests

--- tests/unit/HTMLHelperTest.php
+++ tests/unit/HTMLHelperTest.php
@@ -932,7 +932,7 @@ class HTMLHelperTest extends \Codeception\Test\Unit
 		$this->attributes['max'] = '';
         $this->attributes['formtype'] = 'm13';
         $notEmptyFile = HTMLHelper::formFile($this->attributes);
-        $expected = '<input id="test" type="file" formtype="m13" /><label for="test">test</label>';
+        $expected = '<input id="test" type="file" formtype="m13"/><label for="test">test</label>';
         $this->assertEquals($expected, $notEmptyFile);
 		
         $this->attributes['label'] = 'test';
