9e5aab22e059191cff51c2522239f7650180507d
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2019-07-12 13:50:12 -0700

    update circleci config, remove compiled css files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,6 @@
 const mix = require('laravel-mix');
 
 mix.sass('public/assets/sass/app.scss', 'public/assets/css') // you can "chain" more source source files.
-.sass('public/assets/sass/app1.scss', 'public/assets/css'). //app1.scss is an example
    options({
       includePaths: [
         path.resolve(__dirname, 'public/assets/sass/')
