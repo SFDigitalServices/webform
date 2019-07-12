d9ce23cb75e74f7086cdbc3e0d7a308938c1bb12
Josh Rubenoff <josh@rubenoff.com>
2019-07-12 15:23:56 -0700

    DFB-328: Organize and reformat CSS (#89)
    * Remove unused styles.css
    
    * Reformat editor.css for consistency
    
    No content / style changes
    
    * Reorder styles according to features, add comments
    
    * Move .tabbable under lefthand pane header
    
    * Explain form-wide action menu
    
    * Incorporate changes from #85 and #87
    
    * Convert editor.css to nested SCSS
    
    * Incorporate stylesheet changes in #86
    
    * Ignore compiled app.css
    
    * Add includePaths setting to fix imports
    
    * Use path.resolve for includePaths
    
    * need to remove app.css from gitignore, circleci needs to push this file back to branch after build
    
    * Trigger deployment
    
    * Trigger deployment
    
    * testing skip circleci build, [ci skip]
    
    * update circleci config, remove compiled css files
    
    * fix error in webpack mix
    
    * Trigger deployment [ci skip]
    
    * update circleci config
    
    * Final test, added bunch of white spaces to editor.scss
    
    * move scss linting before compiling
    
    * need to update linting rules to properly format empty lines
    
    * Trigger deployment [ci skip]

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,11 @@
 const mix = require('laravel-mix');
 
-mix.sass('public/assets/sass/app.scss', 'public/assets/css') // you can "chain" more source source files.
-.sass('public/assets/sass/app1.scss', 'public/assets/css'). //app1.scss is an example
+mix.sass('public/assets/sass/app.scss', 'public/assets/css'). // you can "chain" more source source files.
    options({
-      autoprefixer: {}, /* use default options */
+      includePaths: [
+        path.resolve(__dirname, 'public/assets/sass/')
+      ],
+      autoprefixer: {} /* use default options */
       /*postCss: [ // install more postCss plugins: https://github.com/postcss/postcss/blob/master/docs/plugins.md
          require("stylelint")({  }), // auto fix is not supported when used as postCss plugin, moved it to CLI
          ]
