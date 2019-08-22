89dc074826f3811f27655996dcaae201bc055f9b
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2019-08-22 10:23:16 -0700

    merge

--- tests/frontend/step_definitions/steps.js
+++ tests/frontend/step_definitions/steps.js
@@ -1,24 +1,18 @@
-//const CONFIG = require('my.config.js').myConfig;
+// const CONFIG = require('my.config.js').myConfig;
 
-const loginPage = require('../pages/login');
-const I = actor();
+const loginPage = require('../pages/login')
+const I = actor()
 
-// Add in your custom step files
-
-// you can provide RegEx to match corresponding steps
-  Given(/I have a formbuilder account/, () => {
-  I.amOnPage(process.env.CODECEPT_URL || 'http://localhost');
-});
-
-// or a simple string
+// Basic Feature, you can provide RegEx to match corresponding steps
+Given(/I have a formbuilder account/, () => {
+  I.amOnPage(process.env.CODECEPT_URL || 'http://localhost')
+})
 When('I logged into the my account', () => {
-  loginPage.sendForm('johndoe@example.com','johndoe');
-});
-
-// parameters are passed in via Cucumber expressions
+  loginPage.sendForm('johndoe@example.com', 'johndoe')
+})
 Then('I should see all my forms', () => {
-  I.see("Welcome back");
-});
+  I.see('Welcome back')
+})
 Then('I should be able to create new forms', () => {
-  I.see('Create a New Form');
-});
+  I.see('Create a New Form')
+})
