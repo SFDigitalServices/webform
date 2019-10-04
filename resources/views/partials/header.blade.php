8ac9043d2d794a4ed547e6f5beef659a36a6cc46
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-10-04 14:18:40 -0700

    Shorter header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,6 @@
 <div class="header">
   <div class="header-container">
-    <div class="logo">
-      <a href="javascript:void(0)" onclick="fb.goHome()">
-        <img src="/assets/images/SF_Digital_Services-logo.png" alt="SF Digital Services Logo" />
-      </a>
-    </div>
-    Webform Builder
+    <h1>Digital Services Webform Builder</h1>
     <a href="/home" class="header-sign-out btn btn-default">Sign Out</a>
   </div>
 </div>
