37cac0b49a6c16d08996824b99a73d481bc8cc01
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-10-04 15:17:45 -0700

    Convert header link to a button

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <div class="header">
   <div class="header-container">
-    <h1>Digital Services Webform Builder</h1>
+    <button onclick="fb.goHome()">Digital Services Webform Builder</button>
     <a href="/home" class="header-sign-out btn btn-default">Sign Out</a>
   </div>
 </div>
