b850a987ce897e897f3852e87351bc0f3361fedb
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-10-07 10:26:08 -0700

    Chain webpack source files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.sass('public/assets/sass/app.scss', 'public/assets/css'). // you can "chain" more source source files.
-   options({
-      includePaths: [
-        path.resolve(__dirname, 'public/assets/sass/')
-      ],
-      autoprefixer: {} /* use default options */
-      /*postCss: [ // install more postCss plugins: https://github.com/postcss/postcss/blob/master/docs/plugins.md
-         require("stylelint")({  }), // auto fix is not supported when used as postCss plugin, moved it to CLI
-         ]
-         */
-});
-
-mix.sass('public/assets/sass/form-preview.scss', 'public/assets/css'). // you can "chain" more source source files.
+mix.sass('public/assets/sass/app.scss', 'public/assets/css').
+    sass('public/assets/sass/form-preview.scss', 'public/assets/css').
    options({
       includePaths: [
         path.resolve(__dirname, 'public/assets/sass/')
