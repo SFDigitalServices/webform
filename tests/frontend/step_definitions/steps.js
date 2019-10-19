69034f1fe438503d115e7831c112a776cd568ae5
Jim Brodbeck <jim.brodbeck@sfgov.org>
2019-10-18 18:01:24 -0700

    fix port config

--- tests/frontend/step_definitions/steps.js
+++ tests/frontend/step_definitions/steps.js
@@ -5,7 +5,7 @@ const I = actor();
 
 // you can provide RegEx to match corresponding steps
   Given(/I have a formbuilder account/, () => {
-  I.amOnPage(process.env.CODECEPT_URL || 'http://localhost:8000');
+  I.amOnPage(process.env.CODECEPT_URL || 'http://localhost');
 });
 
 // Basic Feature
