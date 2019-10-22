a9e9276e79da6d7f405d976436e445fe77c117ae
Jim Brodbeck <jim.brodbeck@sfgov.org>
2019-10-21 18:00:57 -0700

    test

--- tests/frontend/step_definitions/all_steps.js
+++ tests/frontend/step_definitions/all_steps.js
@@ -15,9 +15,9 @@ Before(() => {
 // Create Form Feature
 Given(/I am on the dashboard page/, () => {
   //loginPage.login('johndoe@example.com','johndoe')
-  I.wait(15)
+  I.waitForElement("a.start-form", 30)
   I.click('Create a New Form')
-  I.wait(5)
+  I.wait(2)
   I.see('Please enter the name of your form')
   I.fillField({id: 'formTitle'}, 'TESTING FORM CREATION'+sessid)
   I.click('Ok')
