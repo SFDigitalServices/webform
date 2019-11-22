a31cb794f6fe2b4c83251c8c148e1d7670a71634
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2019-11-22 10:32:38 -0800

    update path for sass import

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,18 +8,19 @@ mix.copy(
       'public/assets/sass/sf-design-system/layout/').
     copy(
       'node_modules/sf-design-system/src/components/04-forms/**/*.scss',
-      'public/assets/sass/sf-design-system/forms/');
+      'public/assets/sass/sf-design-system/forms/').then() => {
 
-  mix.sass('public/assets/sass/app.scss', 'public/assets/css').
-    sass('public/assets/sass/form-branding.scss', 'public/assets/css').
-    sass('public/assets/sass/form-preview.scss', 'public/assets/css').
-   options({
-      includePaths: [
-        path.resolve(__dirname, 'public/assets/sass/')
-      ],
-      autoprefixer: {} /* use default options */
-      /*postCss: [ // install more postCss plugins: https://github.com/postcss/postcss/blob/master/docs/plugins.md
-         require("stylelint")({  }), // auto fix is not supported when used as postCss plugin, moved it to CLI
-         ]
-         */
-});
+        mix.sass('public/assets/sass/app.scss', 'public/assets/css').
+          sass('public/assets/sass/form-branding.scss', 'public/assets/css').
+          sass('public/assets/sass/form-preview.scss', 'public/assets/css').
+          options({
+            includePaths: [
+              path.resolve(__dirname, 'public/assets/sass/')
+            ],
+            autoprefixer: {} /* use default options */
+            /*postCss: [ // install more postCss plugins: https://github.com/postcss/postcss/blob/master/docs/plugins.md
+              require("stylelint")({  }), // auto fix is not supported when used as postCss plugin, moved it to CLI
+              ]
+              */
+        });
+      });
