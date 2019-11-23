f63897b5395568faaf9fb6505684651b7311ff23
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-11-22 16:00:12 -0800

    Add Henry’s Webpack / copy updates

---
+++ scripts/copy_sf_design_system.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo cp node_modules/sf-design-system/src/components/00-design-tokens/*.scss public/assets/sass/sf-design-system/design-tokens
+sudo cp node_modules/sf-design-system/src/components/03-layout/**/*.scss public/assets/sass/sf-design-system/layout/
+sudo cp node_modules/sf-design-system/src/components/04-forms/**/*.scss public/assets/sass/sf-design-system/forms/
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,15 @@
 const mix = require('laravel-mix');
 
-mix.copy(
-      'node_modules/sf-design-system/src/components/00-design-tokens/**/*.scss',
-      'public/assets/sass/sf-design-system/design-tokens/').
-    copy(
-      'node_modules/sf-design-system/src/components/03-layout/**/*.scss',
-      'public/assets/sass/sf-design-system/layout/').
-    copy(
-      'node_modules/sf-design-system/src/components/04-forms/**/*.scss',
-      'public/assets/sass/sf-design-system/forms/').then( () => {
-        mix.sass('public/assets/sass/app.scss', 'public/assets/css').
-          sass('public/assets/sass/form-branding.scss', 'public/assets/css').
-          sass('public/assets/sass/form-preview.scss', 'public/assets/css').
-          options({
-            includePaths: [
-              path.resolve(__dirname, 'public/assets/sass/')
-            ],
-            autoprefixer: {} /* use default options */
-            /*postCss: [ // install more postCss plugins: https://github.com/postcss/postcss/blob/master/docs/plugins.md
-              require("stylelint")({  }), // auto fix is not supported when used as postCss plugin, moved it to CLI
-              ]
-              */
-        });
-      });
+  mix.sass('public/assets/sass/app.scss', 'public/assets/css').
+    sass('public/assets/sass/form-branding.scss', 'public/assets/css').
+    sass('public/assets/sass/form-preview.scss', 'public/assets/css').
+    options({
+      includePaths: [
+        path.resolve(__dirname, 'public/assets/sass/')
+      ],
+      autoprefixer: {} /* use default options */
+      /*postCss: [ // install more postCss plugins: https://github.com/postcss/postcss/blob/master/docs/plugins.md
+        require("stylelint")({  }), // auto fix is not supported when used as postCss plugin, moved it to CLI
+        ]
+        */
+  });
