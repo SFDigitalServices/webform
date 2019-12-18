3454bea570a01ff58f145504da6b4351d423dd6d
Jim Brodbeck <jim.brodbeck@sfgov.org>
2019-12-17 17:23:21 -0800

    code review requested changes

--- public/assets/js/error-msgs.js
+++ public/assets/js/error-msgs.js
@@ -1,5 +1,6 @@
 var SFDSerrorMsgs = function() {
-	jQuery('#SFDSWF-Container input[required], #SFDSWF-Container select[required] #SFDSWF-Container textarea[required]').attr('data-error','This field cannot be blank.');
+	jQuery('#SFDSWF-Container input[required], #SFDSWF-Container textarea[required]').attr('data-error','This field cannot be blank.');
+	jQuery('#SFDSWF-Container select[required]').attr('data-error','You need to select an item in the list.');
 	jQuery('#SFDSWF-Container input[type=tel]').attr('data-error','You need to enter a valid phone number.'); //doesn't seem to validate phone numbers
 	jQuery('#SFDSWF-Container input[type=email]').attr('data-error','You need to enter a valid email address.');
 	jQuery('#SFDSWF-Container input[type=number]').attr('data-error','You need to enter a number.'); //does not work as a type error
