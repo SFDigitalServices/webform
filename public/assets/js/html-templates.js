3d08ae751a22d1682fcebade44693c376a550bc8
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-12-30 13:14:15 -0800

    Remove “placeholder” and “default value” options

--- public/assets/js/html-templates.js
+++ public/assets/js/html-templates.js
@@ -116,7 +116,6 @@ Fb.prototype.view.editItem = "\
 			<div class='accordion'> \
 				<input type='hidden' name='formtype' id='formtype'/>\
 				<label class='control-label label-attribute'>Label</label> <input class='form-control label-attribute' type='text' name='label' id='label'> \
-				<label class='control-label placeholder-attribute'>Placeholder</label> <input type='text' name='placeholder' id='placeholder' class='form-control placeholder-attribute'> \
 				<label class='control-label help-attribute'>Description</label> <textarea class='form-control help-attribute' name='help' id='help'></textarea> <p class='help-block'>Tell residents what they need to know to answer your question correctly.</p>\
 				<label class='control-label option-attribute'>Options</label> <textarea class='form-control option-attribute' name='option' id='option'> </textarea> \
 				<label class='control-label checkboxes-attribute'>Options</label> <textarea class='form-control checkboxes-attribute' name='checkboxes' id='checkboxes'> </textarea> \
@@ -124,7 +123,6 @@ Fb.prototype.view.editItem = "\
 				<label class='control-label textarea-attribute'>Content</label> <textarea class='form-control textarea-attribute' name='textarea' id='textarea'></textarea> \
 				<label class='control-label codearea-attribute'>Content</label> <textarea class='form-control codearea-attribute' name='codearea' id='codearea'></textarea> \
 				<label class='control-label unit-attribute'>Unit</label> <input type='text' name='unit' id='unit'> \
-				<label class='control-label value-attribute'>Default alue</label> <input type='text' name='value' id='value'> \
 				<label class='control-label name-attribute'>Name attribute</label> <input class='form-control name-attribute' type='text' name='name' id='name'> <p class='help-block'>The field's label in your database. Users will never see this.</p>\
 				<label class='control-label id-attribute'>ID</label> <input class='form-control id-attribute' type='text' name='id' id='id'> \
 				<label class='control-label class-attribute'>CSS classes</label> <input class='form-control class-attribute' type='text' name='class' id='class'> <p class='help-block'>To add multiple classes, just add spaces.</p>\
