d13e6828175e92091cef2993b1b84b10887b2056
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2020-01-22 17:02:07 -0800

    Add confirmation email partials

---
+++ resources/views/emails/field-multi.blade.php
@@ -0,0 +1,20 @@
+{{-- Checkboxes, radio buttons, dropdowns --}}
+
+  <td align="left" bgcolor="#FFFFFF" width="100%" style="font-size: 14px; line-height: 18px; font-family: 'Rubik', sans-serif; font-weight: 400;  color: #1C3E57; padding-top: 34px;">{{ $label }}</td>
+</tr>
+<tr>
+  <td align="left" bgcolor="#FFFFFF" width="100%">
+  	<table role="presentation">
+
+      {{-- Repeat this <tr> for every checked option --}}
+      <tr>
+        <td align="left" valign="top" style="padding-top: 9px;">
+          <img src="{{ $formbuilderURL }}/assets/images/email-checkbox.jpg" alt="" />
+        </td>
+        <td align="left" valign="top" style="font-size: 17px; line-height: 24px; font-family: 'Rubik', sans-serif; font-weight: 400;  color: #1C3E57; padding: 6px 0px 6px 15px;">{{ $option_1 }}.</td>
+      </tr>
+      {{-- End <tr> --}}
+
+    </table>
+  </td>
+</tr>
