e932e9e7372ddff6895f0f60cb2c98456145105f
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2020-01-22 17:14:51 -0800

    Fix typos and whitespace issues

--- resources/views/emails/partial/field-link.blade.php
+++ resources/views/emails/partial/field-link.blade.php
@@ -1,4 +1,4 @@
-  {{ -- URL --}}
+  {{-- URL --}}
   <tr>
     <td align="left" bgcolor="#FFFFFF" width="100%" style="font-size: 14px; line-height: 18px; font-family: 'Rubik', sans-serif; font-weight: 400;  color: #1C3E57; padding-top: 34px;">{{ label }}</td>
   </tr>
