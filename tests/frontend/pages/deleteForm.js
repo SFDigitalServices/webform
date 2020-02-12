469bf4fef6ae7d7769746291e1acd8eeb94a3e89
Jim Brodbeck <jim.brodbeck@sfgov.org>
2020-02-12 15:53:25 -0800

    adjust e2e test

--- tests/frontend/pages/deleteForm.js
+++ tests/frontend/pages/deleteForm.js
@@ -18,10 +18,9 @@ module.exports = {
     })
     Then('I should be redirected back to the dashboard', () => {
       I.waitForText('Welcome back,', 3, '.content')
-      I.wait(10)
     })
-    Then('I should not see the deleted form', () => {
-      I.dontSee('TESTING FORM CREATION'+sessid)
+    Then('I should not see the deleted form', async () => {
+      await I.dontSee('TESTING FORM CREATION'+sessid)
     })
     When('I create a new form', async () => {
       I.waitForText('Please enter the name of your form')
