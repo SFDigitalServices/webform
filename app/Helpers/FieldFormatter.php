a4111b300e9c562e1d813a8e9f4f0e1e6a614e92
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2020-03-05 14:01:29 -0800

    update field format

--- app/Helpers/FieldFormatter.php
+++ app/Helpers/FieldFormatter.php
@@ -56,6 +56,7 @@ class FieldFormatter
     public static function formatOptions($name, $value)
     {
         $checklist = '';
+        // need to move this to the design system
         $checkmark = '<img src="https://svgsilh.com/svg/40319.svg" height="25px" width="25px" />';
         if(is_array($value)){
           $count = 0;
