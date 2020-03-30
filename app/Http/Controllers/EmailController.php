7c400679bea83d1eae039f3d0ccd5eef559ff30f
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2020-03-30 15:48:16 -0700

    update from-address to use sf.gov address

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -11,8 +11,8 @@ class EmailController extends Controller
     public function sendEmail($data, $template)
     {
         $data['emailInfo']['template'] = isset($data['emailInfo']['template']) ? $data['emailInfo']['template'] : $template;
-        $data['emailInfo']['from_address'] = isset($data['emailInfo']['address']) ? $data['emailInfo']['address'] : 'donotreply@sf.gov';
-        $data['emailInfo']['replyto_address'] = isset($data['emailInfo']['address']) ? $data['emailInfo']['address'] : 'donotreply@sf.gov';
+        $data['emailInfo']['from_address'] = isset($data['emailInfo']['from_address']) ? $data['emailInfo']['from_address'] : getenv("MAIL_FROM_ADDRESS");
+        $data['emailInfo']['replyto_address'] = isset($data['emailInfo']['replyto_address']) ? $data['emailInfo']['replyto_address'] : getenv("MAIL_REPLYTO_ADDRESS");
         $data['emailInfo']['subject'] = isset($data['emailInfo']['subject']) ? $data['emailInfo']['subject'] : 'Form submissions status';
         $data['emailInfo']['name'] = isset($data['emailInfo']['name']) ? $data['emailInfo']['name'] : 'City and County of San Francisco';
         if ($data['emailInfo']['address'] !== '') {
